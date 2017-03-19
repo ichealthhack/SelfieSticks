a018f6c7f8f8a77cf7fe51bb562fa7e06237a771
Javi Geis <javigeis@dyn1231-229.wlan.ic.ac.uk>
2017-03-19 12:49:31 +0000

    added base functions to main

---
+++ Project1/Assets/Scripts/loseCond.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loseCond : MonoBehaviour {
+
+	public GameObject textPrefab;
+	public GameObject canvas;
+	private GameObject textEnd;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider colider) {
+		Debug.Log("detect colision");
+		textEnd = Instantiate(textPrefab);
+		textEnd.transform.SetParent(canvas.GetComponent<Transform> ());
+		textEnd.transform.localPosition = new Vector3(0.5f,0.5f,0.5f);
+	}
+}
